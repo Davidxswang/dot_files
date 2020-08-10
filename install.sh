8d147d2a8d2d083b6fa79cb4b0f76646834d3e28
David Wang <wxs199327@hotmail.com>
2020-08-10 11:02:23 -0700

    update install

--- install.sh
+++ install.sh
@@ -11,14 +11,12 @@ cp vimrc ~/.vimrc
 echo -e "\e[93mThe .vimrc file is copied to ~.\e[0m"
 
 # tmux.conf
-if [ -a ~/tmux.conf ]
+if [ -a ~/.tmux.conf ]
 	then
 		echo -e "\e[91mA tmux.conf file already exists in ~, a backup is made.\e[0m"
-		cp ~/tmux.conf ~/tmux.conf.backup
+		cp ~/.tmux.conf ~/.tmux.conf.backup
 fi
 
-cp tmux.conf ~/tmux.conf
+cp tmux.conf ~/.tmux.conf
 echo -e "\e[91mThe tmux.conf file is copied to ~.\e[0m"
-tmux source-file ~/tmux.conf
-echo -e "\e[91mThe tmux.conf is loaded.\e[0m"
 
