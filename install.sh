e55e68b8989741965afb46c72c1e181abc189825
David Wang <wxs199327@hotmail.com>
2021-01-26 15:13:29 +0800

    add jupyter notebook

--- install.sh
+++ install.sh
@@ -23,8 +23,17 @@ echo -e "\e[93mThe tmux.conf file is copied to ~.\e[0m"
 # jupyter lab
 if [ -a ~/.jupyter/jupyter_lab_config.py ]
 	then
-		echo -e "\e[91mA jupyter_lab_config.py file already exists in ~, a backup is made.\e[0m"
+		echo -e "\e[91mA jupyter_lab_config.py file already exists in ~/.jupyter/, a backup is made.\e[0m"
 		cp ~/.jupyter/jupyter_lab_config.py ~/.jupyter/jupyter_lab_config.py.backup
 fi
 cp jupyter_lab_config.py ~/.jupyter/jupyter_lab_config.py
 echo -e "\e[93mThe jupyter_lab_config.py file is copied to ~/.jupyter/"
+
+# jupyter notebook
+if [ -a ~/.jupyter/jupyter_notebook_config.py ]
+	then
+		echo -e "\e[91mA jupyter_notebook_config.py file already exists in ~/.jupyter/, a backup is made.\e[0m"
+		cp ~/.jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py.backup
+fi
+cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+echo -e "\e[93mThe jupyter_notebook_config.py file is copied to ~/.jupyter/"
