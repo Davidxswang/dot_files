1f0c9fcf440ee7371f68dd024cb1effab6c86a97
Xuesong Wang <wxs199327@hotmail.com>
2021-04-08 10:45:50 +0800

    make folder ~/.jupyter if no already exist

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # .vimrc
-if [ -a ~/.vimrc ]
+if [ -f ~/.vimrc ]
 	then
 		echo -e "\e[91mA .vimrc file already exists in ~, a backup is made.\e[0m"
 		cp ~/.vimrc ~/.vimrc.backup
@@ -11,7 +11,7 @@ cp vimrc ~/.vimrc
 echo -e "\e[93mThe .vimrc file is copied to ~.\e[0m"
 
 # tmux.conf
-if [ -a ~/.tmux.conf ]
+if [ -f ~/.tmux.conf ]
 	then
 		echo -e "\e[91mA tmux.conf file already exists in ~, a backup is made.\e[0m"
 		cp ~/.tmux.conf ~/.tmux.conf.backup
@@ -20,20 +20,29 @@ fi
 cp tmux.conf ~/.tmux.conf
 echo -e "\e[93mThe tmux.conf file is copied to ~.\e[0m"
 
+# jupyter config folder
+if [ ! -d "~/.jupyter" ]
+	then
+		echo -e "\e[93m~/.jupyter folder does not exist, so we created it.\e[0m"
+		mkdir ~/.jupyter
+fi
+
 # jupyter lab
-if [ -a ~/.jupyter/jupyter_lab_config.py ]
+if [ -f ~/.jupyter/jupyter_lab_config.py ]
 	then
 		echo -e "\e[91mA jupyter_lab_config.py file already exists in ~/.jupyter/, a backup is made.\e[0m"
 		cp ~/.jupyter/jupyter_lab_config.py ~/.jupyter/jupyter_lab_config.py.backup
 fi
+
 cp jupyter_lab_config.py ~/.jupyter/jupyter_lab_config.py
 echo -e "\e[93mThe jupyter_lab_config.py file is copied to ~/.jupyter/"
 
 # jupyter notebook
-if [ -a ~/.jupyter/jupyter_notebook_config.py ]
+if [ -f ~/.jupyter/jupyter_notebook_config.py ]
 	then
 		echo -e "\e[91mA jupyter_notebook_config.py file already exists in ~/.jupyter/, a backup is made.\e[0m"
 		cp ~/.jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py.backup
 fi
+
 cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 echo -e "\e[93mThe jupyter_notebook_config.py file is copied to ~/.jupyter/"
